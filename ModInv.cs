9f9aaefb73b66612e4345dff0494de116d6223c1
Marcus Newman <marcusgeorgenewman@gmail.com>
2018-06-16 12:31:47 -0700

    working for ints now

--- ModInv.cs
+++ ModInv.cs
@@ -1,8 +1,28 @@
 ﻿using System;
 
-namespace MGN.ModInv
-{
+namespace MGN
+{ 
     public class ModInv
     {
+        public static int? Calculate(int value, int modulus)
+        {
+            int m0 = modulus;
+            int y = 0, x = 1;
+            while (value > 1)
+            {
+                if (modulus == 0) return null;
+                int quotient = value / modulus;
+
+                int t = modulus;
+                modulus = value % modulus;
+                value = t;
+                t = y;          
+                y = x - quotient * y;
+                x = t;
+            }
+            if (x < 0)
+            x += m0;
+            return x;
+        }
     }
 }
--- Tests/ModInvTests.cs
+++ Tests/ModInvTests.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reflection;
 
 namespace MGN.ModInv.Tests
 {
@@ -8,7 +10,68 @@ namespace MGN.ModInv.Tests
         [TestMethod]
         public void MGN_ModInv_dllShouldExist()
         {
-            ReflectionAssert.AssemblyExists("MGN.ModInv.dll");
+            GetAssembly();
+        }
+
+        private static Assembly GetAssembly()
+        {
+            return ReflectionAssert.AssemblyExists("MGN.ModInv.dll");
+        }
+
+        [TestMethod]
+        public void ModInvTypeShouldExist()
+        {
+
+            GetModInvType();
+        }
+
+        private static Type GetModInvType()
+        {
+            var assembly = GetAssembly();
+            return assembly.TypeExists("ModInv", "MGN");
+        }
+
+        [TestMethod]
+        public void CalculateMethodShouldExist()
+        {
+            GetCalculateMethod();
+
+        }
+
+        private static MethodInfo GetCalculateMethod()
+        {
+            var modInvType = GetModInvType();
+            return modInvType.MethodExists("Calculate",
+                shouldBeStatic: true,
+                shouldReturnType: typeof(int?),
+                shouldBeAnExtentionMethod: false,
+                parameterTypesAndNames: new System.Collections.Generic.List<Tuple<Type, string>> {
+                    Tuple.Create(typeof(int), "value"),
+                    Tuple.Create(typeof(int), "modulus") });
+        }
+
+        [TestMethod]
+        public void inv15mod26shouldBe7()
+        {
+            var value = 15;
+            var modulus = 26;
+            var ExpectedResult = 7;
+            TestMethod(value, modulus, ExpectedResult);
+        }
+
+        private static void TestMethod(int value, int modulus, int? ExpectedResult)
+        {
+            ReflectionAssert.TestMethod(GetCalculateMethod(),
+                            null, new object[] { value, modulus }, ExpectedResult);
+        }
+
+        [TestMethod]
+        public void inv2mod12shouldBeNull()
+        {
+            var value = 2;
+            var modulus = 12;
+            int? ExpectedResult = null;
+            TestMethod(value, modulus, ExpectedResult);
         }
     }
 }
